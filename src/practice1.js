b328e0f793da8dc48ce8fe0e3d848fd710bd5593
Wen Fu <faith.WilliamFu@gmail.com>
2020-10-18 18:13:23 +0800

    [Wen Fu] completed homework.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) => {
+    let columns = [];
+    input.column.map((name)=>{
+        columns.push(name.name);
+    })
+
+    return input.data.map(person => {
+       return Object.assign({}, ...(person.map((value, index) => {
+            return  {[columns[index]] : value}
+        })))
+    })
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+    sections.map((section, index) => {
+        items.splice(section.index + index, 0, section.content)
+    })
+
+    return items;
+}
 export { inject };
